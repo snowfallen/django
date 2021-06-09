be434703109304d1652fa6a89dc5bb07cd21dcbc
Carlton Gibson <carlton.gibson@noumenal.es>
2021-06-09 20:32:58 +0200

    Refs #30427 -- Removed outdated invalid model test.

--- tests/invalid_models_tests/test_models.py
+++ tests/invalid_models_tests/test_models.py
@@ -1208,21 +1208,6 @@ class OtherModelTests(SimpleTestCase):
             )
         ])
 
-    def test_property_and_related_field_accessor_clash(self):
-        class Model(models.Model):
-            fk = models.ForeignKey('self', models.CASCADE)
-
-        # Override related field accessor.
-        Model.fk_id = property(lambda self: 'ERROR')
-
-        self.assertEqual(Model.check(), [
-            Error(
-                "The property 'fk_id' clashes with a related field accessor.",
-                obj=Model,
-                id='models.E025',
-            )
-        ])
-
     def test_single_primary_key(self):
         class Model(models.Model):
             foo = models.IntegerField(primary_key=True)
